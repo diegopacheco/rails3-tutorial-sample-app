6c7731103a533894bf5234b014e8ba0db566ce9f
Diego Pacheco <diego.pacheco.it@gmail.com>
2011-08-04 23:55:51 -0300

    Done with cap 9 but no exerc

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,9 +40,13 @@ class User < ActiveRecord::Base
    end
 
    def self.authenticate(email, submitted_password)
-      user = find_by_email(submitted_password)  
-      return nil  if user.nil?
-      return user if user.has_password?(submitted_password)
+      user = find_by_email(email)
+      user && user.has_password?(submitted_password)? user : nil
+   end
+
+   def self.authenticate_with_salt(id,cookie_salt)
+      user = find_by_id(id)
+      (user && user.salt == cookie_salt) ? user: nil
    end
 
    def email_address
